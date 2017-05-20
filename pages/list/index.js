5dbdb89e21156fb3c3319769362c42a3fec7ca97
cooleye <iscooleye@163.com>
2017-05-20 09:14:48 +0800

    add

--- pages/list/index.js
+++ pages/list/index.js
@@ -11,7 +11,27 @@ Page({
     autoplay: false,
     interval: 2000,
     duration: 500,
-    circular:true
+    circular: true,
+    workds: [
+      {
+        title: '积云教育',
+        logo: '',
+        subtitle: '培养年薪20万的技术专家##',
+        skills: ['1.好好学习，天天向上1', '2.好好学习，天天向上2', '3.好好学习，天天向上3', '4.好好学习，天天向上4', '5.好好学习，天天向上5', '6.好好学习，天天向上6']
+      },
+      {
+        title: '积云教育',
+        logo: '',
+        subtitle: '培养年薪20万的技术专家',
+        skills: ['1.好好学习，天天向上', '2.好好学习，天天向上', '3.好好学习，天天向上', '4.好好学习，天天向上', '5.好好学习，天天向上', '6.好好学习，天天向上']
+      },
+      {
+        title: '积云教育',
+        logo: '',
+        subtitle: '培养年薪20万的技术专家',
+        skills: ['1.好好学习，天天向上', '2.好好学习，天天向上', '3.好好学习，天天向上', '4.好好学习，天天向上', '5.好好学习，天天向上', '6.好好学习，天天向上']
+      }
+    ]
   },
   changeIndicatorDots: function (e) {
     this.setData({
@@ -38,69 +58,69 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   }
 })
